dfb026a2fbbfd8355e73db3a0838867f728e93c5
Geoff Root <groot314@gmail.com>
2023-10-27 10:51:28 -0400

    Positions cleanup

--- src/main/java/com/levelup/forestsandmonsters/Position.java
+++ src/main/java/com/levelup/forestsandmonsters/Position.java
@@ -5,9 +5,12 @@ import java.awt.Point;
 public class Position {
 
     public Point point;
+
+    private static int DEFAULT_X = 4;
+    private static int DEFAULT_Y = 4;
     
     public Position(){
-        point = new Point(4,4);
+        point = new Point(DEFAULT_X, DEFAULT_Y);
     }
 
     public Position(int x, int y){
--- src/test/java/com/levelup/forestsandmonsters/PositionTest.java
+++ src/test/java/com/levelup/forestsandmonsters/PositionTest.java
@@ -1,19 +1,31 @@
 package com.levelup.forestsandmonsters;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.awt.Point;
 
 import org.junit.Test;
 
 public class PositionTest {
     
+
+    private static int TEST_X = 4;
+    private static int TEST_Y = 4;
+
     @Test
     public void initPosition(){
-        Position position = new Position(4,4);
-
+        Position position = new Position(TEST_X,TEST_Y);
          assertNotNull(position);
     }
 
-    
+    @Test
+    public void initXPosition(){
+        Position pos = new Position(TEST_X,TEST_Y);
+        assertEquals(TEST_X, pos.point.getX());
+    }
+
+    @Test
+    public void initYPosition(){
+        Position pos = new Position(TEST_X,TEST_Y);
+        assertEquals(TEST_Y, pos.point.getY());
+    }
 }
